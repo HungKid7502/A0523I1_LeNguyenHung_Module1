0fea98682851613daac88dd9a247ef2b83192f17
hungkid7502 <hunglnde160537@fpt.edu.vn>
2023-06-22 22:19:06 +0700

    new project

--- ss7_Bien_KieuDuLieu_ToanTu/Ứng dụng máy tính/Caculate.js
+++ ss7_Bien_KieuDuLieu_ToanTu/Ứng dụng máy tính/Caculate.js
@@ -2,26 +2,26 @@ addition() {
     let a = parseFloat(document.getElementById("number1").value);
     let b = parseFloat(document.getElementById("number2").value);
     r = a + b;
-    document.getElementById("result").innerHTML = (r);
+    document.getElementById("result").innerHTML = r;
 }
 
 subtraction() {
     let a = parseFloat(document.getElementById("number1").value);
     let b = parseFloat(document.getElementById("number2").value);
     r = a - b;
-    document.getElementById("result").innerHTML = (r);
+    document.getElementById("result").innerHTML = r;
 }
 
 multiplication() {
     let a = parseFloat(document.getElementById("number1").value);
     let b = parseFloat(document.getElementById("number2").value);
     r = a * b;
-    document.getElementById("result").innerHTML = (r);
+    document.getElementById("result").innerHTML = r;
 }
 
 division() {
     let a = parseFloat(document.getElementById("number1").value);
     let b = parseFloat(document.getElementById("number2").value);
     r = a / b;
-    document.getElementById("result").innerHTML = (r);
+    document.getElementById("result").innerHTML = r;
 }
